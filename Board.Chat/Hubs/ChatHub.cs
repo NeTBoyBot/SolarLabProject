c988dd67201a36d90981384cba448af364a652b1
NeTBoyBot <toolumini@mail.ru>
2023-04-26 17:32:55 +0300

    Add SignalR

---
+++ Board.Chat/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Board.Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string username,string message)
+        {
+            await Clients.All.SendAsync("Send",username, message);
+        }
+    }
+}
